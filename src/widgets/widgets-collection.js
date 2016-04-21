f125ecde86583f99968899755f9ae4df69c25481
Francisco Dans <fdansv@gmail.com>
2016-04-21 19:19:21 +0200

    handles color scale change in collection

--- src/widgets/widgets-collection.js
+++ src/widgets/widgets-collection.js
@@ -15,12 +15,21 @@ module.exports = Backbone.Collection.extend({
     this.bind('change:order', _.debounce(this._onChangeOrder.bind(this), 10), this);
     // If a category model applies the category colors, rest should remove/disable
     // the category colors applied before.
-    this.bind('change:isAutoStyle', function (m, isColorCategorized) {
+    this.bind('change:autoStyle', function (m, isColorCategorized) {
       if (isColorCategorized) {
         this.each(function (widgetModel) {
           // Only set if model actually has the attr (i.e. it's a category model)
-          if (m !== widgetModel && _.isBoolean(widgetModel.get('isAutoStyle'))) {
-            widgetModel.set('isAutoStyle', false);
+          if (m !== widgetModel && _.isBoolean(widgetModel.get('autoStyle'))) {
+            widgetModel.set('autoStyle', false);
+          }
+        });
+      }
+    }, this);
+    this.bind('change:colorScale', function (m, isColorCategorized) {
+      if (isColorCategorized) {
+        this.each(function (widgetModel) {
+          if (m !== widgetModel && widgetModel.has('colorScale')) {
+            widgetModel.set('colorScale', null);
           }
         });
       }
